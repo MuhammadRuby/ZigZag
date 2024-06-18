e68b056c138a011d099e730513a3989adcefdb42
mohamed ali <mohammed4ziny@gmail.com>
2024-06-18 21:15:15 +0000

    mohammed ali commit

--- main.py
+++ main.py
@@ -19,10 +19,14 @@ model = YOLO(MODEL)
 # dict mapping class_id to class_name
 CLASS_NAMES_DICT = model.model.names
 # get class id ball and bottle
-CLASS_ID = [32, 39]
+CLASS_ID = [32, 39,0]
+PLAYER_CLASS_ID = 0
+BOTTLE_CLASS_ID = 39  
+BALL_CLASS_ID = 32    
 
-SOURCE_VIDEO_PATH = f"dataset\zigzag.mp4"
-TARGET_VIDEO_PATH = f"dataset\zigzag_result.mp4"
+
+SOURCE_VIDEO_PATH = f"dataset/zigzag.mp4"
+TARGET_VIDEO_PATH = f"dataset/zigzag_result.mp4"
 
 # create VideoInfo instance
 video_info = VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
@@ -56,52 +60,77 @@ with VideoSink(TARGET_VIDEO_PATH, video_info) as sink:
             tracker_id=tracker_ids
         )
 
-        # filter detections for ball and bottle
-        mask = np.array([class_id in CLASS_ID for class_id in detections.class_id], dtype=bool)
-        detections.filter(mask=mask, inplace=True)
-        print("\n")
+        # filter detections for bottles
+        mask_bottles = np.array([class_id == BOTTLE_CLASS_ID for class_id in detections.class_id], dtype=bool)
+        bottle_detections = Detections(
+            xyxy=detections.xyxy[mask_bottles],
+            confidence=detections.confidence[mask_bottles],
+            class_id=detections.class_id[mask_bottles],
+            tracker_id=detections.tracker_id[mask_bottles]
+        )
+        # print(bottle_detections.tracker_id)
+        # break
+        
         # Get keypoints for the person
-        keypoints = results_poses[0].keypoints.xy.int().cpu().tolist()
-        if len(keypoints) > 0:
-            player_position = keypoints[0][0]  # Assuming keypoint[0][0] is the player's center point
-            print(f"player has been found at {player_position}")
+        mask_player = np.array([class_id == PLAYER_CLASS_ID for class_id in detections.class_id], dtype=bool)
+        player_detections = Detections(
+            xyxy=detections.xyxy[mask_player],
+            confidence=detections.confidence[mask_player],
+            class_id=detections.class_id[mask_player],
+            tracker_id=detections.tracker_id[mask_player]
+        )
+        keypoints = player_detections.xyxy
+            
         # Detect bottles and 
         bottles_detected = [detections.xyxy[i] for i, class_id in enumerate(detections.class_id) if CLASS_NAMES_DICT[class_id] == "bottle"]
-        # # check if any bottle falls
-        # if any((bottle[2] - bottle[0]) > (bottle[3] - bottle[1]) for bottle in bottles_detected):
-        #     print("Fail: A bottle has fallen")
-        #     break
-
-        
+  # # check if any bottle falls
+  # if any((bottle[2] - bottle[0]) > (bottle[3] - bottle[1]) for bottle in bottles_detected):
+  #     print("Fail: A bottle has fallen")
+  #     break
         # #searching for nearest bottle before checking crossing line
-        if len(bottles_detected) >= 2 and  not found_nearest_bottle:
-            nearest_bottle = time_estimator.set_start_bottle(player_position, bottles_detected)
-            time_estimator.set_start_bottle_position(nearest_bottle)
-            found_nearest_bottle = True
-            
-            print("nearest bottle found")
+       
+        if len(keypoints) > 0:
+            for player in keypoints:
+              player_position = int((player[0]+player[2])/2)  # Assuming keypoint[0][0] is the player's center point            
+              print(f"player has been found at {player_position}")
+              if len(bottles_detected) >= 2 and  not found_nearest_bottle:
+                nearest_bottle = time_estimator.set_start_bottle(player_position, bottle_detections)
+                start_x = time_estimator.update_bottle_position(nearest_bottle,bottle_detections)
+                found_nearest_bottle = True
+              print(f"nearest bottle found at {start_x}")
 
-        #check if crossed the line and get start timer
-        if time_estimator.crossed_start_bottle_line(player_position) and start_time is None and found_nearest_bottle:
-            start_time = time.time()    
-            time_started = True
-            # time_estimator.update(player_position, bottles_detected)
-            print(f"started at {start_time}")
-            print("timer has been started")
+               
 
-        #check if crossed the line end get end time
-        if time_estimator.crossed_start_bottle_line(player_position) and time.time() > start_time + 5 and found_nearest_bottle:
-            end_time = time.time()
-            time_taken = (end_time - start_time)/10
-            print(f"stopped at {end_time}")
-            print("timer has been stopped !! ")
-            break
+              #check if crossed the line and get start timer
+              if time_estimator.crossed_start_bottle_line(player_position) and start_time is None and found_nearest_bottle:
+                  start_time = time.time()    
+                  time_started = True
+                  # time_estimator.update(player_position, bottles_detected)
+                  print(f"started at {start_time}")
+                  print("timer has been started") 
 
+              #check if crossed the line end get end time
+              if time_estimator.crossed_start_bottle_line(player_position) and time.time() > start_time + 5 and found_nearest_bottle:
+                  end_time = time.time()
+                  time_taken = (end_time - start_time)/10
+                  print(f"stopped at {end_time}")
+                  print(f"timer has been stopped afer {time_taken} !")
+                  break
+              cv2.circle(annotated_frame,(player_position,0),20, (255,0,200),20)
+        
+        n_bottle_x = time_estimator.update_bottle_position(nearest_bottle,bottle_detections)
         # annotate and display frame
+        # cv2.circle(annotated_frame,(player_position,0),20, (255,0,200),20)
+        print(f"nearest bottle found at {start_x}")    
+        if n_bottle_x is not None:
+           start_x = n_bottle_x
+           time_estimator.set_start_line_position(start_x)
+        cv2.circle(annotated_frame,(int(start_x),0),20, (255,200,0),20)
+                
         labels = [
             f"id:{track_id} {CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
             for i, (xyxy, confidence, class_id, track_id) in enumerate(zip(detections.xyxy, detections.confidence, detections.class_id, detections.tracker_id))
-        ]
+          ]
         annotated_frame = box_annotator.annotate(frame=annotated_frame, detections=detections, labels=labels)
         time_annotator = TimeAnnotator(start_time, end_time)
         time_annotator.annotate(annotated_frame, time_estimator)
--- utils.py
+++ utils.py
@@ -3,33 +3,59 @@ import cv2
 import numpy as np
 from supervision.draw.color import Color
 from supervision.geometry.dataclasses import Point
+import time
+from typing import List, Tuple
 
 class TimeEstimator:
     def __init__(self):
         self.positions = []
         self.start_bottle_position = None
         self.start_bottle_line = None
+        self.n_bottle_index = None
 
-    def set_start_bottle(self, player_position: Point, bottle_positions: List[np.ndarray]) -> np.ndarray:
-        distances = [np.linalg.norm(np.array(player_position) - np.array([(bottle[0] + bottle[2]) / 2, (bottle[1] + bottle[3]) / 2])) for bottle in bottle_positions]
+
+    # def set_start_bottle(self, player_position: Point, bottle_positions: List[np.ndarray]) -> np.ndarray:
+    #     distances = [np.linalg.norm(np.array(player_position) - np.array([(bottle[0] + bottle[2]) / 2, (bottle[1] + bottle[3]) / 2])) for bottle in bottle_positions]
+    #     nearest_bottle_index = np.argmin(distances)
+    #     self.n_bottle_index = nearest_bottle_index
+    #     return bottle_positions[nearest_bottle_index]
+    def set_start_bottle(self, player_position: Point, bottle_detections) -> int:
+        # Calculate distances from the player to the center of each bottle
+        distances = [
+            np.linalg.norm(np.array(player_position) - np.array([(bottle[0][0] + bottle[0][2]) / 2, (bottle[0][1] + bottle[0][3]) / 2])) 
+            for bottle in bottle_detections
+        ]
+        # Find the index of the nearest bottle
         nearest_bottle_index = np.argmin(distances)
-        return bottle_positions[nearest_bottle_index]
+        self.n_bottle_index = nearest_bottle_index
+        # Return the tracker ID of the nearest bottle
+        return bottle_detections.tracker_id[nearest_bottle_index]
 
-    def set_start_bottle_position(self, bottle: np.ndarray):
-        self.start_bottle_position = bottle
-        bottle_center_x = (bottle[0] + bottle[2]) / 2
+    def update_bottle_position(self, tracker_id: int, bottle_detections: List[Tuple[List[float], float, int, int]]) -> Point:
+        # Find the detection with the specified tracker ID
+        for index,ID in enumerate(bottle_detections.tracker_id):
+            if ID == tracker_id:
+                # Calculate and return the center position of the bottle
+                bottle = bottle_detections.xyxy[index]
+                bottle_center_x = (bottle[0] + bottle[2]) / 2
+                self.start_bottle_line= bottle_center_x
+                return bottle_center_x
+        # If the tracker ID is not found, return None or raise an exception
+        return None
+
+    def set_start_line_position(self,bottle_center_x ):
         self.start_bottle_line = bottle_center_x
+        return self.start_bottle_line
 
-    def update(self, player_position: Point, bottle_positions: List[np.ndarray]) -> bool:
-        self.positions.append(player_position)
-        return True
+    def update_nearest_bottle(self,bottle_positions: List[np.ndarray]):
+        return bottle_positions[self.n_bottle_index]
 
     def crossed_start_bottle_line(self, player_position: Point) -> bool:
         if self.start_bottle_line is None:
             return False
         
-        current_player_x = player_position[0]
-        return current_player_x <= self.start_bottle_line
+        current_player_x = player_position
+        return  self.start_bottle_line -5 <= current_player_x <= self.start_bottle_line+5
 
 class TimeAnnotator:
     def __init__(
@@ -54,9 +80,15 @@ class TimeAnnotator:
 
     def annotate(self, frame: np.ndarray, time_estimator: TimeEstimator) -> np.ndarray:
         if self.start_time is not None and self.end_time is not None:
-            time_text = f"Time: {self.end_time - self.start_time:.2f} seconds"
+            time_text = f"Time: {(self.end_time - self.start_time)/10:.2f} seconds"
+        elif self.start_time is not None:
+
+          # time_text = f"Time: {time.time() - self.start_time:.2f} seconds"
+          current_time  = (time.time() - self.start_time)/10
+          time_text = f"Time: {current_time:0.2f} seconds"
         else:
-            time_text = "Tracking..."
+          time_text = f"Time: tracking ....."
+
 
         (text_width, text_height), _ = cv2.getTextSize(
             time_text, cv2.FONT_HERSHEY_SIMPLEX, self.text_scale, self.text_thickness
